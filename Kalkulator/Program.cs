47c2282ae50e021a87fac45051231ee35aa4d46c
Kuba <genekmaj@wp.pl>
2019-09-22 15:30:01 +0200

    Introduce menu to application

---
+++ Kalkulator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1. Dodawanie");
+                Console.WriteLine("2. Odejmowanie");
+
+                int menuOption = Convert.ToInt32(Console.ReadLine());
+
+                if (menuOption == 0)
+                {
+
+                    break;
+                }
+            }
+        }
+    }
+}
